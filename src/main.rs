e4f7b252c1d089bf7ee4bcaec052a596c4d6c9e4
Erwann <erwannlj>
2024-02-18 19:18:47 +0100

    config file auto creation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io::Write;
+use std::path::Path;
+use std::env;
+
 fn main() {
+
+    creat_congig();
+
+
     let url = "156.67.224.173";
     let path = "/record/current.jpg";
 
@@ -7,4 +15,19 @@ fn main() {
         .unwrap()
         .copy_to(&mut file)
         .unwrap();
+}
+
+fn creat_congig(){
+    let mire = "https://upload.wikimedia.org/wikipedia/commons/1/1b/RCA_Indian_Head_test_pattern.JPG";
+    
+    let key = "HOME";
+    let home = env::var(key).expect("Variable HOME indefinie");
+    let path = format!("{}{}", home, "/.config/wallbrother/");
+    let full_path = format!("{}{}", home, "/.config/wallbrother/wallbrother.conf");
+    
+    std::fs::create_dir_all(path).unwrap();
+    if !Path::new("does_not_exist.txt").exists() {
+        let mut conf = std::fs::File::create(full_path).unwrap();
+        conf.write_all(format!("mire    0   {}", mire).as_bytes()).unwrap();
+    }
 }
